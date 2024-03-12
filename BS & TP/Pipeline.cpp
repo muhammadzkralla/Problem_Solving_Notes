cfd277464cc72630d58ab6f37758a1e7d68e8d0f
Zkrallah <muhammad.heshamyt@gmail.com>
2024-03-12 15:35:41 +0200

    Pipeline

---
+++ BS & TP/Pipeline.cpp
@@ -0,0 +1,71 @@
+/**
+/$$$$$$
+|_  $$_/
+  | $$    /$$$$$$/$$$$   /$$$$$$    /$$$$$$     /$$$$$$    /$$$$$$$   /$$$$$$$
+  | $$   | $$_  $$_  $$ /$$__  $$  /$$__  $$   /$$__  $$  /$$_____/  /$$_____/
+  | $$   | $$ \ $$ \ $$ | $$  \ $$ | $$  \__/ | $$$$$$$$ |  $$$$$$  |  $$$$$$
+  | $$   | $$ | $$ | $$ | $$  | $$ | $$       | $$_____/  \____  $$  \____  $$
+ /$$$$$$ | $$ | $$ | $$ | $$$$$$$/ | $$       |  $$$$$$$  /$$$$$$$/  /$$$$$$$/
+|______/ |__/ |__/ |__/ | $$____/  |__/        \_______/ |_______/  |_______/
+                        | $$
+                        | $$
+                        |__/
+ **/
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <chrono>
+#include <numeric>
+#define ll long long
+
+using namespace std;
+auto mod = 1e9 + 7;
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+struct node {
+    int x;
+    int y;
+    bool operator<(const node& other) const {
+        return (x < other.x) || (x == other.x && y < other.y);
+    }
+};
+
+void solve() {
+    ll n, k, ans = -1;
+    cin >> n >> k;
+    ll l = 0, r = k, all = (k * (k - 1) / 2) + 1;
+    while (l <= r) {
+        ll mid = l + (r - l) / 2;
+        ll diff = (mid * (mid - 1) / 2);
+
+        if (all - diff >= n) {
+            ans = k - mid;
+            l = mid + 1;
+        } else r = mid - 1;
+    }
+
+    cout << ans << endl;
+
+}
+
+int main() {
+    fast();
+    int t = 1;
+    while (t--){
+        solve();
+    }
+}
--- BS & TP/README.md
+++ BS & TP/README.md
@@ -4,3 +4,4 @@
 [102961G Sum of Two Values](https://codeforces.com/problemset/gymProblem/102961/G "102961G Sum of Two Values") <br>
 [102961ZA Subarray Sums I](https://codeforces.com/problemset/gymProblem/102961/ZA "102961ZA Subarray Sums I") <br>
 [102961H Maximum Subarray Sum](https://codeforces.com/problemset/gymProblem/102961/H "102961H Maximum Subarray Sum") <br>
+[287B Pipeline](https://codeforces.com/problemset/problem/287/B "287B Pipeline") <br>
