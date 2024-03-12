3864098c7005dc68b094f016956ff2c57298a400
Zkrallah <muhammad.heshamyt@gmail.com>
2024-03-12 15:06:13 +0200

    Factory Machines

---
+++ BS & TP/Factory Machines.cpp
@@ -0,0 +1,78 @@
+/**
+/$$$$$$
+|_  $$_/
+  | $$    /$$$$$$/$$$$   /$$$$$$    /$$$$$$     /$$$$$$    /$$$$$$$   /$$$$$$$
+  | $$   | $$_  $$_  $$ /$$__  $$  /$$__  $$   /$$__  $$  /$$_____/  /$$_____/
+  | $$   | $$ \ $$ \ $$ | $$  \ $$ | $$  \__/ | $$$$$$$$ |  $$$$$$  |  $$$$$$
+  | $$   | $$ | $$ | $$ | $$  | $$ | $$       | $$_____/  \____  $$  \____  $$
+ /$$$$$$ | $$ | $$ | $$ | $$$$$$$/ | $$       |  $$$$$$$  /$$$$$$$/  /$$$$$$$/
+|______/ |__/ |__/ |__/ | $$____/  |__/        \_______/ |_______/  |_______/
+                        | $$
+                        | $$
+                        |__/
+ **/
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <chrono>
+#include <numeric>
+#define ll long long
+
+using namespace std;
+auto mod = 1e9 + 7;
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+struct node {
+    int x;
+    int y;
+    bool operator<(const node& other) const {
+        return (x < other.x) || (x == other.x && y < other.y);
+    }
+};
+
+void solve() {
+    ll n, t;
+    cin >> n >> t;
+    vector<ll> v(n);
+    for (int i = 0; i < n; ++i) cin >> v[i];
+
+    ll l = 1, r = 1e18, ans = r;
+
+    while (l <= r) {
+        ll mid = l + (r - l) / 2, tmp = 0;
+        for (int i = 0; i < n; ++i) {
+            tmp += mid / v[i];
+            if (tmp >= t) {
+                ans = mid;
+                break;
+            }
+        }
+
+        if (ans == mid) r = mid - 1;
+        else l = mid + 1;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    fast();
+    int t = 1;
+    while (t--){
+        solve();
+    }
+}
--- BS & TP/README.md
+++ BS & TP/README.md
@@ -1,1 +1,2 @@
-[706B Interesting Drink](https://github.com/muhammadzkralla/ZHttp_Demo "706B Interesting Drink")
+[706B Interesting Drink](https://codeforces.com/problemset/problem/706/B/ "706B Interesting Drink")
+[102961U Factory Machines](https://codeforces.com/problemset/gymProblem/102961/U "102961U Factory Machines")
